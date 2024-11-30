ffbc951bc3c4dcce1b63072f64c31fea80312de5
Ramer Alegre Ramos <ramzxs@gmail.com>
2024-11-30 22:12:46 +0800

    Intro

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,25 @@
-/* CLASS */
-/* Blueprint of an object */
-/* Modifier, class Keyword, Classname */
+/* CLASS
+Blueprint of an object
+Modifier, class Keyword, Classname */
+
+import java.util.Scanner;
+
 public class HelloWorld {
     /* FUNCTION */
     /* Function Name, (Parameters List), {Body} */
     
     /* main() Function Signature */
     // Entry point of a Java app
+    // C/C++: int main() { }
     public static void main(String[] args) {
         // Object-Oriented Programming Language
         // All statements start with an object
         System.out.print("Hello, World!");
+
+        Scanner kb = new Scanner(System.in);  // Wait for  user input
+        kb.nextLine();
+
+        // Deploying Java Apps
+        // JAR: Java Archive File (apk, exe)
     }
 }
---
+++ basics/Intro.java
@@ -0,0 +1,31 @@
+package basics; // No need if you did not create 'basics' folder
+
+// import *
+
+public class Intro {
+    public static void main(String[] args) { // main() Method/Function Signature
+        // Procedural Programming
+        // step1();
+        // step2();
+        // step3();
+
+        // Object-Oriented Programming
+        // obj1.step1();
+        // obj1.step2();
+        // obj1.step3();
+
+        // Strings
+        // \n New Line
+        // \t Tab
+        System.out.print("String 1\n");
+        System.out.println("String 2"); // \n at the end
+        System.out.println("String 3");
+
+        // Character
+        // '' is just for 1 character
+        System.out.println('A');
+
+        // Concatenate strings using + operator
+        System.out.println("String 1 " + '|' + " String 2"); // "String 1 | String 2"
+    }
+}
